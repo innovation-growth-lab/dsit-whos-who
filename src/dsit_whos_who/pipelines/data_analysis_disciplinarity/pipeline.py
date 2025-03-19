a4a045a272f7fd9c32f305d2327e3f8b5740edd2
ampudia19 <dampudiavicente@gmail.com>
2025-03-19 14:54:17 +0100

    tmp remove pipeline disc

--- src/dsit_whos_who/pipelines/data_analysis_disciplinarity/pipeline.py
+++ src/dsit_whos_who/pipelines/data_analysis_disciplinarity/pipeline.py
@@ -20,38 +20,38 @@ def create_pipeline(**kwargs) -> Pipeline:  # pylint: disable=W0613
     Returns:
         Pipeline: The data collection pipeline.
     """
-    collection_pipeline = pipeline(
-        [
-            node(
-                func=create_list_oa_author_ids,
-                inputs="ad.matched_authors.primary",
-                outputs="analysis.disciplinarity.oa.list",
-                name="create_oa_author_ids",
-            ),
-            node(
-                func=fetch_openalex,
-                inputs={
-                    "perpage": "params:disciplinarity.oa.api.perpage",
-                    "mails": "params:disciplinarity.oa.api.mails",
-                    "ids": "analysis.disciplinarity.oa.list",
-                    "filter_criteria": "params:disciplinarity.oa.filter",
-                    "parallel_jobs": "params:disciplinarity.oa.n_jobs",
-                    "endpoint": "params:disciplinarity.oa.publications_endpoint",
-                    "keys_to_include": "params:disciplinarity.oa.keys_to_include",
-                },
-                outputs="analysis.disciplinarity.oa.raw",
-                name="fetch_oa_publications",
-            ),
-            node(
-                func=concatenate_openalex_publications,
-                inputs={
-                    "data": "analysis.disciplinarity.oa.raw",
-                    "gtr_publications": "oa.data_collection.publications.intermediate",
-                },
-                outputs="analysis.disciplinarity.oa.intermediate",
-                name="concatenate_openalex_publications",
-            ),
-        ]
-    )
+    # collection_pipeline = pipeline(
+    #     [
+    #         node(
+    #             func=create_list_oa_author_ids,
+    #             inputs="ad.matched_authors.primary",
+    #             outputs="analysis.disciplinarity.oa.list",
+    #             name="create_oa_author_ids",
+    #         ),
+    #         node(
+    #             func=fetch_openalex,
+    #             inputs={
+    #                 "perpage": "params:disciplinarity.oa.api.perpage",
+    #                 "mails": "params:disciplinarity.oa.api.mails",
+    #                 "ids": "analysis.disciplinarity.oa.list",
+    #                 "filter_criteria": "params:disciplinarity.oa.filter",
+    #                 "parallel_jobs": "params:disciplinarity.oa.n_jobs",
+    #                 "endpoint": "params:disciplinarity.oa.publications_endpoint",
+    #                 "keys_to_include": "params:disciplinarity.oa.keys_to_include",
+    #             },
+    #             outputs="analysis.disciplinarity.oa.raw",
+    #             name="fetch_oa_publications",
+    #         ),
+    #         node(
+    #             func=concatenate_openalex_publications,
+    #             inputs={
+    #                 "data": "analysis.disciplinarity.oa.raw",
+    #                 "gtr_publications": "oa.data_collection.publications.intermediate",
+    #             },
+    #             outputs="analysis.disciplinarity.oa.intermediate",
+    #             name="concatenate_openalex_publications",
+    #         ),
+    #     ]
+    # )
 
-    return collection_pipeline
+    return pipeline([])
